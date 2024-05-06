76f03cda430b8ff86a1c8c007f861dcd25ff9139
Estevan <estevan.crispin@gmail.com>
2024-05-05 23:53:14 -0600

    working on mongoose

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-require ('dotenv').config();
-const restaurants = require('./routes/resturants.js');
-const reviews = require('./routes/reviews.js')
-const HappyHour = require('./routes/HappyHourMenu.js')
+const mongoose = require("mongoose");
 
-
+require("dotenv").config();
+const restaurants = require("./routes/resturants.js");
+const reviews = require("./routes/reviews.js");
+const HappyHour = require("./routes/HappyHourMenu.js");
 
 //middleware
-app.use(express.json())
-app.use((req, res, next)=>{
-   console.log(req.path, req.method)
-   next();
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-})
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome! If you want to access any of the routes, please use the URL: /users, /meals, or /comments"
+  );
+});
 
+app.use("/api/resturants", restaurants);
+app.use("/api/reviews", reviews);
+app.use("/api/happyhourf", HappyHour);
 
-app.get('/', (req, res) => {
-    res.send('Welcome! If you want to access any of the routes, please use the URL: /users, /meals, or /comments');
+// connect to DB
+mongoose.connect(process.env.Atlas_URI);
+  .then(() =>{
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening at http://localhost:`, process.env.PORT);
+    });
+
   });
+  .catch((error){ =>
+    console.log(error)
+  });
+  
 
-  app.use('/api/resturants', restaurants);
-  app.use('/api/reviews', reviews);
-  app.use('/api/happyhourf', HappyHour);
-
-  app.listen(process.env.PORT, () => {
-    console.log(`App listening at http://localhost:`, process.env.PORT)
-  })
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const reviews = require('../Db/reviews.js');
+// const restaurants = require('../Db/resturants.js');
 
+//get all restaurtants
 router.get('/', (req, res) => {
-        res.send(reviews);
+        res.json({mssg: 'get all resturants'});
     });
+
+// get single restuarant
+router.get('/:id', (req,res)=>{
+    res.json({mssg: 'get a single resturant'})
+});
+
+//post new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'post new workout'});
+});
+// delete a workout
+router.delete('/:id', (req,res)=>{
+    res.json({mssg: 'delete a workout'})
+});
+// update a workout
+router.patch('/:id', (req,res)=>{
+    res.json({mssg: 'update a  resturant'})
+});
+
 module.exports = router
